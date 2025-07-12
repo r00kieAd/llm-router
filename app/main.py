4ad75bd32fe2b96756e6330b9473798c3ae048c6
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2025-07-13 01:24:00 +0530

    added openai client

--- app/main.py
+++ app/main.py
@@ -1,24 +1,16 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from app.services.gemini_client import query_gemini
+from app.services.model_router import route_to_client
 
 app = FastAPI()
+
 
 class AskRequest(BaseModel):
     prompt: str
     client: str
+    model: str
+
 
 @app.post("/ask")
 async def ask(request: AskRequest):
-    if request.client == "gemini":
-        response = query_gemini(request.prompt)
-        return {
-            "response": response,
-            "model_used": "gemini-2.5-flash"
-        }
-    else:
-        response = "api test succeeded"
-        return {
-            "response": response,
-            "model_used": "none"
-        }
+    return route_to_client(request.prompt, request.client, request.model)
