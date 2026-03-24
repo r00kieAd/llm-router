c9779c56b999303c46e2fe68c04b8e3f35a3de03
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2026-03-25 00:41:13 +0530

    added stream response headers

--- app/services/gemini_client.py
+++ app/services/gemini_client.py
@@ -7,23 +7,6 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-# def query_gemini(prompt: str, model: str, instruction: str, temperature: float, top_p: float, top_k: int, max_output_token: int) -> str:
-#     try:
-#         response = client.models.generate_content(
-#             model=model,
-#             config=types.GenerateContentConfig(
-#                 system_instruction=instruction,
-#                 temperature=temperature,
-#                 topP=top_p,
-#                 topK=top_k,
-#                 maxOutputTokens=max_output_token
-#                 ),
-#             contents=prompt
-#         )
-#         return response.text
-#     except Exception as e:
-#         return f"[Gemini Error] {str(e)}"
-
 def query_gemini_stream(prompt: str, model: str, instruction: str, temperature: float, top_p: float, top_k: int, max_output_token: int):
 
     try:
